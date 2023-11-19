19a50a69e79ac66c28bbdcfdc4937f18d6279829
Patrick Good <patrick.good@ost.ch>
2023-11-19 16:09:05 +0100

    Added IRQ functionalities to GPIO

--- subdevices/flink_gpio.py
+++ subdevices/flink_gpio.py
@@ -15,12 +15,61 @@ class FlinkGPIO(flink.FlinkSubDevice):
         dev = flink.FlinkDevice()
         subDev = dev.getSubdeviceByType(flink.Definitions.GPIO_INTERFACE_ID)
         super().__init__(dev, subDev)
+        dev.lib.flink_dio_get_baseclock.argtypes = [ct.c_void_p, ct.POINTER(ct.c_uint32)]
+        dev.lib.flink_dio_get_baseclock.restype = ct.c_int
         dev.lib.flink_dio_set_direction.argtypes = [ct.c_void_p, ct.c_uint32, ct.c_uint8]
         dev.lib.flink_dio_set_direction.restype = ct.c_int
         dev.lib.flink_dio_set_value.argtypes = [ct.c_void_p, ct.c_uint32, ct.c_uint8]
         dev.lib.flink_dio_set_value.restype = ct.c_int  
         dev.lib.flink_dio_get_value.argtypes = [ct.c_void_p, ct.c_uint32, ct.POINTER(ct.c_uint8)]
         dev.lib.flink_dio_get_value.restype = ct.c_int
+        dev.lib.flink_dio_set_debounce.argtypes = [ct.c_void_p, ct.c_uint32, ct.c_uint32]
+        dev.lib.flink_dio_set_debounce.restype = ct.c_int
+        dev.lib.flink_dio_get_debounce.argtypes = [ct.c_void_p, ct.c_uint32, ct.POINTER(ct.c_uint32)]
+        dev.lib.flink_dio_get_debounce.restype = ct.c_int
+        self._BASE_CLOCK = self._getBaseclock()
+
+    ##################################################################################
+    # Internal methodes
+    ##################################################################################
+    def _getBaseclock(self) -> int:
+        """
+        --> Internal method. NOT recomended to use this function directly!!! <--
+
+        Returns the base clock of the underlying hardware.
+        
+        Parameters
+        ----------
+        channel : channel number 
+        value : false = input, true = output
+        
+        Returns
+        -------
+        Baseclock in Hz
+        """
+        baseClock = ct.c_uint32()
+        error = self.dev.lib.flink_dio_get_baseclock(self.subDev, ct.byref(baseClock))
+        if error < 0:
+            raise flink.FlinkException("Failed to read the base clock of gpio", error, self.subDev)
+        return int(baseClock.value)
+    
+    ##################################################################################
+    # External methodes
+    ##################################################################################
+    def getBaseclock(self) -> int:
+        """
+        Returns the base clock of the underlying hardware.
+        
+        Parameters
+        ----------
+        channel : channel number 
+        value : false = input, true = output
+        
+        Returns
+        -------
+        Baseclock in Hz
+        """
+        return self._BASE_CLOCK
 
     def setDir(self, channel: int, out: bool) -> None:
         """
@@ -78,4 +127,41 @@ class FlinkGPIO(flink.FlinkSubDevice):
         error = self.dev.lib.flink_dio_set_value(self.subDev, channel, val)
         if error < 0:
             raise flink.FlinkException("Failed to set value of gpio channel", error, self.subDev)
+        
+    def setDebounce(self, channel: int, debounce: int) -> None:
+        """
+        Sets the debounce time of a channel for IRQ functionality.
+        This prevents multiple IRQs from being triggered when the input bounces.
+        
+        Parameters
+        ----------
+        channel : channel number 
+        debounce : debounce time. In multiple of base clock
+        
+        Returns
+        -------
+        None
+        """
+        error = self.dev.lib.flink_dio_set_debounce(self.subDev, channel, debounce)
+        if error < 0:
+            raise flink.FlinkException("Failed to set debounce of gpio channel", error, self.subDev)
+        
+    def getDebounce(self, channel: int) -> int:
+        """
+        Gets the debounce time of a channel for IRQ functionality.
+        This prevents multiple IRQs from being triggered when the input bounces.
+        
+        Parameters
+        ----------
+        channel : channel number 
+        
+        Returns
+        -------
+        Debounce time. In multiple of base clock
+        """
+        debounce = ct.c_uint32()
+        error = self.dev.lib.flink_dio_get_debounce(self.subDev, channel, debounce)
+        if error < 0:
+            raise flink.FlinkException("Failed to get debounce of gpio channel", error, self.subDev)
+        return int(debounce.value)
         
